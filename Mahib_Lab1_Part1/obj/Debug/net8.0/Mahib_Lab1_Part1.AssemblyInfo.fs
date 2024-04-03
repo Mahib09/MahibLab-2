935a5b5b638c89b386916035ae0452b1a861e3a8
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2024-04-03 14:10:29 -0400

    Comments Removed

--- Mahib_Lab1_Part1/obj/Debug/net8.0/Mahib_Lab1_Part1.AssemblyInfo.fs
+++ Mahib_Lab1_Part1/obj/Debug/net8.0/Mahib_Lab1_Part1.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Mahib_Lab1_Part1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d2523cc32a3ed45d83e5bba82caf8ae7544757")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Mahib_Lab1_Part1")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Mahib_Lab1_Part1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
--- Mahib_Lab1_Part2/Program.fs
+++ Mahib_Lab1_Part2/Program.fs
@@ -1,9 +1,6 @@
-﻿// Define the Cuisine discriminated union
-type Cuisine = 
+﻿type Cuisine = 
     | Korean 
     | Turkish
-
-// Define the MovieType discriminated union
 type MovieType =
     | Regular 
     | IMAX 
@@ -11,16 +8,12 @@ type MovieType =
     | RegularWithSnacks 
     | IMAXWithSnacks 
     | DBOXWithSnacks
-
-// Define the Activity discriminated union
 type Activity =
     | BoardGame
     | Chill
     | Movie of MovieType
     | Restaurant of Cuisine
     | LongDrive of int * float
-
-// Function to calculate budget for an activity
 let calculateBudget (activity: Activity) =
     match activity with
     | BoardGame -> 0.0
@@ -38,12 +31,9 @@ let calculateBudget (activity: Activity) =
         | Korean -> 70.0
         | Turkish -> 65.0
     | LongDrive (kilometres, fuelCharge) -> float kilometres * fuelCharge
-
-// Example usage
 let activity1 = Movie RegularWithSnacks
 let activity2 = Restaurant Turkish
 let activity3 = LongDrive (100, 0.08)
-
 printfn "Budget for Activity 1: %.2f CAD" (calculateBudget activity1)
 printfn "Budget for Activity 2: %.2f CAD" (calculateBudget activity2)
 printfn "Budget for Activity 3: %.2f CAD" (calculateBudget activity3)
